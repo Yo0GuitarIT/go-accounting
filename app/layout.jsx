45f8b1317d4dfae00b46cc8713c4206ba05a1d1e
Mac <mac@Yo0-Mac.local>
2023-12-27 16:51:40 +0800

    feat: 🎸 connect with firebase

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
       <html lang="en">
         <body className={inter.className}>
           <Header />
-          <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <main className="flex min-h-screen flex-col items-center justify-between p-24 text-center">
             <Suspense fallback={<Loading />}>{children}</Suspense>
           </main>
         </body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,16 @@
+import Link from "next/link"
+
 function Home() {
   return (
     <div>
       <h1 className="text-2xl font-bold ">Welcome</h1>
-      <p className="mb-5">
-        Demo by next.js & clerk
-      </p>
+      <p className="mb-5">Demo by next.js & clerk</p>
+      <Link href="/main" className="hover:text-rose-600 mr-5">
+        Get Started
+      </Link>
+      <Link href="/test" className="hover:text-rose-600">
+        Test
+      </Link>
     </div>
   );
 }
